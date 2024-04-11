d3518672b96a13fe7bf64f7232d7aeea7872e265
ricardo-temple <ricardovtemple@gmail.com>
2024-04-10 21:27:47 -0300

    Initial Commit

---
+++ src/main/java/org/example/Cp2JavaApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp2JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cp2JavaApplication.class, args);
+    }
+
+}
